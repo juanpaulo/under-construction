7d85b2f002df47684eea8ea7f1a4860e9df2ac6e
Juan Paulo Gutierrez <juanpaulo.gutierrez@gmail.com>
2014-01-29 12:30:58 +0900

    Modified to handle content dynamically. Minor CSS changes.

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,22 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'juanpaulogutierrez.com' });
+  var info = {
+    title: 'juanpaulogutierrez.com',
+    backgroundUrl: 'http://farm8.staticflickr.com/7033/6668797013_bb1dc44269_b.jpg',
+    domain: 'juanpaulogutierrez',
+    tld: '.com',
+    message: 'Launching soon!',
+    snsLinks: [
+      { url: "http://www.facebook.com/juanpaulogutierrez", iconClass: "fa-facebook" },
+      { url: "http://plus.google.com/+JuanPauloGutierrez/about", iconClass: "fa-google-plus" },
+      { url: "http://jp.linkedin.com/pub/juan-paulo-gutierrez/2b/b06/284/", iconClass: "fa-linkedin" },
+      { url: "http://github.com/juanpaulo", iconClass: "fa-github" },
+      { url: "http://twitter.com/jp_gutierrez17", iconClass: "fa-twitter" },
+      { url: "http://www.flickr.com/photos/juan_paulo/", iconClass: "fa-flickr" },
+      { url: "http://instagram.com/juanpaulo", iconClass: "fa-instagram" }
+    ],
+    launchDate: 'April 17, 2014 00:00:00'
+  }
+  res.render('index', info);
 };
